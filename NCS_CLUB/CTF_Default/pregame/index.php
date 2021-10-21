39ccb114251c6cd213743fb935fea3aeffadb10b
Josh Cutting <jcutting164@gmail.com>
2021-10-20 20:49:33 -0400

    Default
    The bare bones site without any flags :D

---
+++ NCS_CLUB/CTF_Default/pregame/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+
+<html>
+<body background="Back.png"
+
+<!-- Title code -->
+<!--  -->
+
+<title>NCS CTF</title>
+<body style="height:4000px;">
+<center>
+<a href="https://www.youtube.com/watch?v=I6eQ78HCGEA&ab_channel=JamilHaciyev">
+<h1 style="color:white;font-size:62px;"> Get Ready! The competition starts soon! </h1>
+</a>
+
+
+
+
+
+
+</center>
+
+
+
+
+
+
+</body>
+</html>
